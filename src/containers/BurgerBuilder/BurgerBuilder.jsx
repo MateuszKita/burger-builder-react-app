6ec0b404000a919f3b9292b49676644d7c199315
Mateusz Kita <mkita.zg@gmail.com>
2018-01-29 22:30:45 +0100

    add ingredient control working

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,8 +1,15 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import Aux from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.2,
+    cheese: 0.3,
+    meat: 0.9,
+    bacon: 0.5
+}
 
 class BurgerBuilder extends Component {
     state = {
@@ -11,14 +18,30 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 3
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = () => {}
 
     render() {
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         )
     }
