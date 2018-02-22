830093c9c6dd82b19d1501882d4c13a4e4afa455
Mateusz Kita <mkita.zg@gmail.com>
2018-02-22 22:02:47 +0100

    handling errors

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axiosOrders'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/wiithErrorHandler'
 
 const INGREDIENT_PRICES = {
     salad: 0.2,
@@ -137,4 +138,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder, axios)
