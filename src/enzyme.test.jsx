b24fc7a3548bff699c261208d5c564dae58172c1
Mateusz Kita <mkita.zg@gmail.com>
2018-02-28 07:58:32 +0100

    describe in enzyme test

--- src/enzyme.test.jsx
+++ src/enzyme.test.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import EnzymeTest from './enzymeTest'
 import { shallow } from 'enzyme'
 
-it('renders', () => {
-    const wrapper = shallow(<EnzymeTest title="Test" />)
-    expect(wrapper.find('p').text()).toEqual('Enzyme Test')
+describe('EnzymeTest', () => {
+    it('renders', () => {
+        const wrapper = shallow(<EnzymeTest title="Test" />)
+        expect(wrapper.find('p').text()).toEqual('Enzyme Test')
+    })
 })
