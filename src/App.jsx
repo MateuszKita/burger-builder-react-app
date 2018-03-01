5ccaeba01c7c108a27448e1a271b37e6f06bb5f3
Mateusz Kita <mkita.zg@gmail.com>
2018-03-01 22:15:54 +0100

    deleted comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,11 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
-  // THIS IS JUST FOR TESTING INTERCEPTORS IN withErrorHandler.jsx
-  // state = {
-  //   show: true
-  // }
-
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({ show: false })
-  //   }, 5000)
-  // }
-
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
         </Layout>
       </div>
     )
